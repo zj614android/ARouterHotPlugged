ebd17ebf15f20a8482bd4e9e3f3217623b12ff0c
2807738720@qq.com <2807738720@qq.com>
2018-05-10 16:40:06 +0800

    Arouter 模块化 热插拔

---
+++ moudle_user/src/main/java/aroutertest/zj/com/moudle_user/UserLaunchAcitivty.java
@@ -0,0 +1,13 @@
+package aroutertest.zj.com.moudle_user;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class UserLaunchAcitivty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_lauch_acitivty);
+    }
+}
---
+++ moudle_user/src/main/java/aroutertest/zj/com/moudle_user/application/UserApplication.java
@@ -0,0 +1,18 @@
+package aroutertest.zj.com.moudle_user.application;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.alibaba.android.arouter.launcher.ARouter;
+
+/**
+ * Created by will on 2018/5/3.
+ */
+
+public class UserApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
